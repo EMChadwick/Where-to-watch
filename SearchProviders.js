60480d501a82450135747006facf18cfa43e55a1
Ed Chadwick <edwardmchadwick@gmail.com>
2022-01-10 16:57:32 +0000

    Getting started
    Got the extension working on Chrome, with icons. Just need to add functionality now.

---
+++ SearchProviders.js
@@ -0,0 +1,6 @@
+let startSearch = document.getElementById("startSearch");
+let targetTitle = document.getElementById("targetTitle");
+startSearch.addEventListener("click", async () => {
+	alert("Yeet");
+});
+
